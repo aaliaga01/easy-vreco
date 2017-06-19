f695cfc07dff05908c4a61adfa28c10c5d88346e
aaliaga <a.aliaga.val@gmail.com>
2017-06-19 14:54:30 -0400

    funcion ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
 function initMap(){
+	//--------------
 	var map = new google.maps.Map(document.getElementById("map"), {
-		zoom:5,
-		center: {lat: -9.1191427, lng: -77.0349046},
+		zoom:17,
+		center: {lat: -33.4569400, lng: -70.6482700},
 		mapTypeControl:false,
 		zoomControl: false,
 		streetViewControl:false
 	});
 
+
+//buscar mi posicion actual
 function buscar(){
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
@@ -33,4 +36,61 @@ var funcionExito = function(posicion) {
 var funcionError = function (error) {
 	alert("Tenemos un problema para encontrar tu ubicación");
 	}
+
+
+
+//	
 }
+/*trazar ruta
+function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: {lat: -33.4569400, lng: -70.6482700}
+        });
+        directionsDisplay.setMap(map);
+
+        document.getElementById('ruta').addEventListener('click', function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var waypts = [];
+        var checkboxArray = document.getElementById('waypoints');
+        for (var i = 0; i < checkboxArray.length; i++) {
+          if (checkboxArray.options[i].selected) {
+            waypts.push({
+              location: checkboxArray[i].value,
+              stopover: true
+            });
+          }
+        }
+
+        directionsService.route({
+          origin: document.getElementById('salida').value,
+          destination: document.getElementById('fin').value,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+            for (var i = 0; i < route.legs.length; i++) {
+              var routeSegment = i + 1;
+              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                  '</b><br>';
+              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });*/
+      
