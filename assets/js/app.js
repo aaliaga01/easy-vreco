381dd794503b93b16d142f558ed26fe9f9581401
aaliaga-casa <a.aliaga.val@gmail.com>
2017-06-20 01:23:41 -0300

    no funciona TOT

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ function initMap(){
         });
         directionsDisplay.setMap(map);
 
-        var input = /** @type {!HTMLInputElement} */(
+        /*var input =  @type {!HTMLInputElement} (
             document.getElementById('origen'));
 
         var infowindow = new google.maps.InfoWindow();
@@ -23,10 +23,10 @@ function initMap(){
         autocomplete.addListener('place_changed', function() {
           infowindow.close();
           marker.setVisible(false);
-        });
+        });*/
 
   
-		document.getElementById("ruta").addEventListener("click", function(){//para que funcione con el boton
+    document.getElementById("ruta").addEventListener("click", function(){//para que funcione con el boton
           calculateAndDisplayRoute(directionsService, directionsDisplay);
         });
 
@@ -69,4 +69,85 @@ function initMap(){
     }
 
 }
+
+
+/*var  autocompletar= function(map){
+    var originInput = document.getElementById('origen');
+    var destinationInput = document.getElementById('destino');
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)});
+
+    var orAutocomplete = new google.maps.places.Autocomplete(originInput, destinationInput);
+        orAutocomplete.bindTo('bounds', map);*/
+
+
+
+/*function AutocompleteDirection(map) {
+        
+        this.map = map;
+        this.originPlaceId = document.getElementById('origen');
+        this.destinationPlaceId = document.getElementById('destino');
+        this.travelMode = 'BICYCLING';
+        var originInput = document.getElementById('origen');
+        var destinationInput = document.getElementById('destino');
+        //var modeSelector = document.getElementById('mode-selector');
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        this.directionsDisplay.setMap(map);
+
+        var originAutocomplete = new google.maps.places.Autocomplete(
+            originInput, {placeIdOnly: true});
+
+        var destinationAutocomplete = new google.maps.places.Autocomplete(
+          destinationInput, {placeIdOnly: true});
+
+       //this.setupClickListener('changemode-walking', 'WALKING');
+       //this.setupClickListener('changemode-transit', 'TRANSIT');
+       //this.setupClickListener('changemode-driving', 'DRIVING');
+
+        //this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+        //this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+      }*/
+
+     /*AutocompleteDirection.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+          }
+         if (mode === 'ORIG') {
+          me.originPlaceId = place.place_id;
+         } 
+          else {
+            me.destinationPlaceId = place.place_id;
+          }
+          me.route();
+        });
+
+      };*/
+
+     /* AutocompleteDirection.prototype.route = function() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+          return;
+        }
+        var me = this;
+
+        this.directionsService.route({
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        }, function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };*/
 
